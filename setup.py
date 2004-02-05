d1d2da4fac3d3a6bca0a1990b5c0ef5e21080cb8
elie <elie>
2004-02-05 12:50:08 +0000

    missing packages added

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@
 
 from distutils.core import setup
 
-setup(name="pysnmpap",
+setup(name="pysnmp-apps",
       version="0.0.1",
       description="A collection of SNMP applications written in Python",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
       packages = [ 'pysnmpap',
-                   'pysnmpap.cli' ],
+                   'pysnmpap.cli',
+                   'pysnmpap.cli.ucd',
+                   'pysnmpap.cli.ucd.proto',                   
+                   'pysnmpap.cli.ucd.carrier',
+                   'pysnmpap.cli.ucd.carrier.udp' ],
       data_files = [ ('/usr/local/bin', ['apps/pysnmpwalk']),
                      ('/usr/local/bin', ['apps/pysnmpbulkwalk']),
                      ('/usr/local/bin', ['apps/pysnmptrapd']), ],
