b99d009b15f4e06ff68f7b443d67a092f4d3c2cd
elie <elie>
2004-11-09 15:41:09 +0000

    version changed to alpha

--- setup.py
+++ setup.py
@@ -3,18 +3,11 @@
 from distutils.core import setup
 
 setup(name="pysnmp-apps",
-      version="0.0.2",
+      version="0.1.0-alpha-0",
       description="A collection of SNMP applications written in Python",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
-      packages = [ 'pysnmpap',
-                   'pysnmpap.cli',
-                   'pysnmpap.cli.ucd',
-                   'pysnmpap.cli.ucd.proto',                   
-                   'pysnmpap.cli.ucd.carrier',
-                   'pysnmpap.cli.ucd.carrier.udp' ],
-      scripts = [ 'apps/pysnmpget', 'apps/pysnmpset',
-                  'apps/pysnmpwalk', 'apps/pysnmpbulkwalk',
-                  'apps/pysnmptrap', 'apps/pysnmptrapd' ],
+      packages = [ 'pysnmp_apps', 'pysnmp_apps.cli' ],
+      scripts = [ 'apps/pysnmpwalk' ],
       license="BSD")
