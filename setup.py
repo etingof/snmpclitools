c8579114c7451af06e7a5ad37496975eb8ae86e3
elie <elie>
2004-11-10 17:09:07 +0000

    version correction

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="pysnmp-apps",
-      version="0.1.0-alpha-0",
+      version="0.1.0-alpha",
       description="A collection of SNMP applications written in Python",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
