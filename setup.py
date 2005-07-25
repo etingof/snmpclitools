b2a80e18fc6577c6651196246fc1a42e03a8376a
elie <elie>
2005-07-25 16:08:20 +0000

    0.2.1a

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name="pysnmp-apps",
-      version="0.1.1a",
-      description="A collection of SNMP applications written on top of PySNMP",
+      version="0.2.1a",
+      description="PySNMP applications",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
       packages = [ 'pysnmp_apps', 'pysnmp_apps.v4',
                    'pysnmp_apps.v4.cli' ],
-      scripts = [ 'tools/pysnmpwalk' ],
+      scripts = [ 'tools/pysnmpget' ],
       license="BSD")
