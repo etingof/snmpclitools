3badecda7dd0ce957af8ca26d9f622c8df002a5d
elie <elie>
2005-07-27 16:15:37 +0000

    * more options added * many fixes in various places

--- pysnmp_apps/cli/mibview.py
+++ pysnmp_apps/cli/mibview.py
@@ -2,6 +2,7 @@
 import os, string
 from pyasn1.type import univ
 from pysnmp_apps.cli import base
+from pysnmp.proto import rfc1902
 from pysnmp import error
 
 # Usage
@@ -12,15 +13,21 @@ MIB options:\n\
    -m MIB[:...]      load given list of MIBs (ALL loads everything)\n\
    -M DIR[:...]      look in given list of directories for MIBs\n\
    -O OUTOPTS        Toggle various defaults controlling output display:\n\
-              b:  do not break OID indexes down\n\
-              e:  print enums numerically\n\
+              q:  removes the equal sign and type information\n\
+              Q:  removes the type information\n\
               f:  print full OIDs on output\n\
-              n:  print OIDs numerically\n\
               s:  print only last symbolic element of OID\n\
               S:  print MIB module-id plus last element\n\
               u:  print OIDs using UCD-style prefix suppression\n\
-              U:  don't print units\n\
+              n:  print OIDs numerically\n\
+              e:  print enums numerically\n\
+              b:  do not break OID indexes down\n\
+              E:  include a \" to escape the quotes in indices\n\
+              X:  place square brackets around each index\n\
+              T:  print value in hex\n\
               v:  print values only (not OID = value)\n\
+              U:  don't print units\n\
+              t:  output timeticks values as raw numbers\n\
    -I INOPTS         Toggle various defaults controlling input parsing:\n\
               h:  don't apply DISPLAY-HINTs\n\
               u:  top-level OIDs must have '.' prefix (UCD-style)\n\
@@ -80,7 +87,7 @@ class __MibViewGenerator(base.GeneratorTemplate):
     # Load MIB modules
     def n_MibFile(self, (snmpEngine, ctx), node):
         mibBuilder = snmpEngine.msgAndPduDsp.mibInstrumController.mibBuilder
-        if lower(node[0].attr) == 'all':
+        if string.lower(node[0].attr) == 'all':
             mibBuilder.loadModules()
         else:
             mibBuilder.loadModules(node[0].attr)
@@ -119,24 +126,21 @@ class __MibViewGenerator(base.GeneratorTemplate):
                 mibViewProxy.buildNumericIndices = 1
                 mibViewProxy.buildAbsoluteName = 1
             elif c == 'e':
-                pass
+                raise error.PySnmpError('Option not implemented')
             elif c == 'b':
                 mibViewProxy.buildNumericIndices = 1
             elif c == 'E':
-                pass
+                mibViewProxy.buildEscQuotes = 1
             elif c == 'X':
-                pass
-            elif c == 'a':
-                pass
-            elif c == 'x':
-                pass
+                mibViewProxy.buildSquareBrackets = 1
             elif c == 'T':
-                pass
+                mibViewProxy.buildHexVals = 1
             elif c == 'v':
                 mibViewProxy.buildValueOnly = 1
             elif c == 'U':
                 mibViewProxy.buildUnits = 0
             elif c == 't':
+                mibViewProxy.buildRawTimeTicks = 1
                 pass
             elif c == 'R':
                 mibViewProxy.buildRawVals = 1
@@ -195,7 +199,10 @@ class MibViewProxy:
     buildEqualSign = 1
     buildTypeInfo = 1
     buildEscQuotes = 0
+    buildSquareBrackets = 0
+    buildHexVals = 0
     buildRawVals = 0
+    buildRawTimeTicks = 0
     buildGuessedStringVals = 1
     buildValueOnly = 0
     buildUnits = 1
@@ -222,7 +229,9 @@ class MibViewProxy:
             apply(mibViewController.mibBuilder.loadModules,
                   self.defaultMibs)
         self.__oidValue = univ.ObjectIdentifier()
-            
+        self.__intValue = univ.Integer()
+        self.__timeValue = rfc1902.TimeTicks()
+        
     def getPrettyOidVal(self, mibViewController, oid, val):
         prefix, label, suffix = mibViewController.getNodeName(oid)
         modName, nodeDesc, _suffix = mibViewController.getNodeLocation(prefix)
@@ -246,9 +255,9 @@ class MibViewProxy:
                 if suffix == (0,):
                     out = out + '.0'
                 else:
-                    rowNode, = apply(
-                        mibViewController.mibBuilder.importSymbols,
-                        mibViewController.getNodeLocation(prefix[:-1])
+                    m, n, s = mibViewController.getNodeLocation(prefix[:-1])
+                    rowNode, = mibViewController.mibBuilder.importSymbols(
+                        m, n
                         )
                     if self.buildNumericIndices:
                         out = out+'.'+string.join(
@@ -257,7 +266,12 @@ class MibViewProxy:
                     else:
                         try:
                             for i in rowNode.getIndicesFromInstId(suffix):
-                                out = out + '.\'%s\'' % i
+                                if self.buildEscQuotes:
+                                    out = out + '.\\\"%s\\\"' % i.prettyOut(i)
+                                elif self.buildSquareBrackets:
+                                    out = out + '.[%s]' % i.prettyOut(i)
+                                else:
+                                    out = out + '.\"%s\"' % i.prettyOut(i)
                         except AttributeError:
                             out = out + '.' + string.join(
                                 map(lambda x: str(x), suffix), '.'
@@ -270,36 +284,47 @@ class MibViewProxy:
         mibNode, = mibViewController.mibBuilder.importSymbols(
             modName, nodeDesc
             )
-        if self.buildTypeInfo:
-            out = out + '%s: ' % val.__class__.__name__
-        try:
-            syntax = mibNode.getSyntaxClone()
-        except AttributeError:
-            syntax = None
-        if syntax is None or self.buildRawVals:
-            out = out + str(val)
+        if hasattr(mibNode, 'syntax'):
+            syntax = mibNode.syntax
+        elif hasattr(mibNode, 'getColumnInitializer'):
+            syntax = mibNode.getColumnInitializer().syntax
         else:
-            try:
-                out = out + syntax.prettyGet(val)
-            except AttributeError:
-                if self.__oidValue.isSuperTypeOf(val):
-                    oid, label, suffix = mibViewController.getNodeName(val)
-                    out = out + string.join(
-                        label+tuple(map(lambda x: str(x), suffix)), '.'
-                        )
-                else:
-                    try:
-                        out = out + str(syntax.clone(val))
-                    except:  # XXX handle constraints violation
-                        out = out + str(val)
-            except:
-                out = out + str(val)
+            syntax = val
+        if self.buildTypeInfo:
+            out = out + '%s: ' % syntax.__class__.__name__
+        if self.buildRawVals:
+            out = out + str(val)
+        elif self.buildHexVals: # XXX make it always in hex?
+            if self.__intValue.isSuperTypeOf(val):
+                out = out + repr(int(val))
+            elif self.__oidValue.isSuperTypeOf(val):
+                out = out + repr(tuple(val))
+            else:
+                out = out + repr(str(val))
+        elif self.__timeValue.isSameTypeWith(val):
+            if self.buildRawTimeTicks:
+                out = out + str(int(val))
+            else: # TimeTicks is not a TC
+                val = int(val)
+                d, m = divmod(val, 8640000)
+                out = out + '%d days ' % d
+                d, m = divmod(m, 360000)
+                out = out + '%d:' % d
+                d, m = divmod(m, 6000)
+                out = out + '%d:' % d
+                d, m = divmod(m, 100)
+                out = out + '%d.%d' % (d, m)
+        elif self.__oidValue.isSuperTypeOf(val):
+            oid, label, suffix = mibViewController.getNodeName(val)
+            out = out + string.join(
+                label+tuple(map(lambda x: str(x), suffix)), '.'
+                )
 
-            if self.buildUnits:
-                try:
-                    out = out + ' %s' % mibNode.getUnits()
-                except:
-                    pass
+        out = out + syntax.prettyOut(val)
+
+        if self.buildUnits:
+            if hasattr(mibNode, 'getUnits'):
+                out = out + ' %s' % mibNode.getUnits()
         return out
     
     def setPrettyOidValue(self, (oid, val, t)):
