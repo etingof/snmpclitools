cefa6dc23a56f3e279e71afc072d5d3266e579be
elie <elie>
2009-08-26 16:22:35 +0000

    API versioning mechanics retired (pysnmp_apps.v4 -> pysnmp_apps).

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name="pysnmp-apps",
-      version="0.2.7a",
+      version="0.2.8a",
       description="PySNMP applications",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
-      packages = [ 'pysnmp_apps', 'pysnmp_apps.v4',
-                   'pysnmp_apps.v4.cli' ],
+      packages = [ 'pysnmp_apps',
+                   'pysnmp_apps.cli' ],
       scripts = [ 'tools/pysnmpget', 'tools/pysnmpset',
                   'tools/pysnmpwalk', 'tools/pysnmpbulkwalk',
                   'tools/pysnmptranslate' ],
