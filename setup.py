1be4f041e6c8627484d60e7928e681b0143f043a
elie <elie>
2010-06-22 16:26:18 +0000

    0.2.10a

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 options = {
     'name': "pysnmp-apps",
-    'version': "0.2.9a",
+    'version': "0.2.10a",
     'description': "PySNMP applications",
     'author': "Ilya Etingof",
     'author_email': "ilya@glas.net ",
