1bad15506a2728a0765bda879268d95bcd54574a
elie <elie>
2010-12-13 15:49:43 +0000

    reworked for better compatibility with different distusils/sertuptools versions

--- setup.py
+++ setup.py
@@ -12,14 +12,24 @@ It's very easy to install it, just type (as root on Linux):
 
 try:
     from setuptools import setup
+    params = {
+        'install_requires': [ 'pysnmp' ],
+        'zip_safe': True
+        }
 except ImportError:
     for arg in sys.argv:
         if string.find(arg, 'egg') != -1:
             howto_install_setuptools()
             sys.exit(1)
     from distutils.core import setup
+    if sys.version_info > (2, 2):
+        params = {
+            'requires': [ 'pysnmp' ]
+            }
+    else:
+        params = {}
 
-options = {
+params.update( {
     'name': 'pysnmp-apps',
     'version': '0.2.11a',
     'description': 'PySNMP applications',
@@ -27,20 +37,19 @@ options = {
     'author_email': 'ilya@glas.net',
     'url': 'http://sourceforge.net/projects/pysnmp/',
     'license': 'BSD',
-    'requires': 'pysnmp',
     'packages': [ 'pysnmp_apps', 'pysnmp_apps.cli' ],
     'scripts': [ 'tools/pysnmpget', 'tools/pysnmpset',
                  'tools/pysnmpwalk', 'tools/pysnmpbulkwalk',
                  'tools/pysnmptrap', 'tools/pysnmptranslate' ]
-  }
+  } )
 
 if "py2exe" in sys.argv:
     import py2exe
     # fix executables
-    options['console'] = options['scripts']
-    del options['scripts']
+    params['console'] = params['scripts']
+    del params['scripts']
     # add files not found my modulefinder
-    options['options'] = {
+    params['options'] = {
         'py2exe': {
             'includes': [
                 'pysnmp.smi.mibs.*',
@@ -49,4 +58,4 @@ if "py2exe" in sys.argv:
             }
         }
 
-apply(setup, (), options)
+apply(setup, (), params)
