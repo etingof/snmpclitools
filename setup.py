a7b8775d632bab637996004f6ae932407a021bfe
elie <elie>
2013-09-21 09:53:41 +0000

    distribute is gone

--- setup.py
+++ setup.py
@@ -26,25 +26,13 @@ Topic :: System :: Networking :: Monitoring
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-def howto_install_distribute():
-    print("""
-   Error: You need the distribute Python package!
-
-   It's very easy to install it, just type (as root on Linux):
-
-   wget http://python-distribute.org/distribute_setup.py
-   python distribute_setup.py
-
-   Then you could make eggs from this package.
-""")
-
 def howto_install_setuptools():
     print("""
    Error: You need setuptools Python package!
 
    It's very easy to install it, just type (as root on Linux):
 
-   wget http://peak.telecommunity.com/dist/ez_setup.py
+   wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
    python ez_setup.py
 
    Then you could make eggs from this package.
@@ -59,10 +47,7 @@ try:
 except ImportError:
     for arg in sys.argv:
         if 'egg' in arg:
-            if sys.version_info[0] > 2:
-                howto_install_distribute()
-            else:
-                howto_install_setuptools()
+            howto_install_setuptools()
             sys.exit(1)
     from distutils.core import setup
     params = {}
