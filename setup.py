bedc5dc35a00302b6523ed4f2cc231fe27ca5834
Ilya Etingof <etingof@gmail.com>
2017-01-13 03:04:21 +0100

    list all supported Python versions

--- setup.py
+++ setup.py
@@ -24,12 +24,19 @@ License :: OSI Approved :: BSD License
 Natural Language :: English
 Operating System :: OS Independent
 Programming Language :: Python :: 2
+Programming Language :: Python :: 2.4
+Programming Language :: Python :: 2.5
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Communications
-Topic :: Software Development :: Libraries :: Python Modules
 Topic :: System :: Monitoring
 Topic :: System :: Networking :: Monitoring
-Topic :: Software Development :: Libraries :: Python Modules
 """
 
 def howto_install_setuptools():
